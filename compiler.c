4ffb1aec319af7e87209b10064752e5d9f4468b2
alphaom33 <91857248+alphaom33@users.noreply.github.com>
2025-01-08 16:13:08 +0000

    what

--- compiler.c
+++ compiler.c
@@ -44,7 +44,15 @@ typedef struct {
   int depth;
 } Local;
 
+typedef enum {
+  TYPE_SCRIPT,
+  TYPE_FUNCTION
+} FunctionType;
+
 typedef struct {
+  ObjFunction* function;
+  FunctionType type;
+
   Local locals[UINT8_COUNT];
   int localCount;
   int scopeDepth;
@@ -55,7 +63,7 @@ Compiler* current = NULL;
 Chunk* compilingChunk;
 
 static Chunk* currentChunk() {
-  return compilingChunk;
+  return &current->function->chunk;
 }
 
 static void errorAt(Token* token, const char* message) {
@@ -143,13 +151,17 @@ static void emitReturn() {
   emitByte(OP_RETURN);
 }
 
-static void endCompiler() {
+static ObjFunction* endCompiler() {
   emitReturn();
+  ObjFunction* function = current->function;
+  
 #ifdef DEBUG_PRINT_CODE
   if (!parser.hadError) {
-    disassembleChunk(currentChunk(), "code");
+    disassembleChunk(currentChunk(), function->name != NULL ? function->name->chars : "<script>");
   }  
 #endif
+
+  return function;
 }
 
 static void beginScope() {
@@ -234,6 +246,7 @@ static uint8_t parseVariable(const char* errorMessage) {
 }
 
 static void markInitialized() {
+  if (current->scopeDepth == 0) return;
   current->locals[current->scopeDepth - 1].depth = current->scopeDepth;
 }
 
@@ -305,10 +318,18 @@ static void patchJump(int offset) {
   currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
-static void initCompiler(Compiler* compiler) {
+static void initCompiler(Compiler* compiler, FunctionType type) {
+  compiler->function = NULL;
+  compiler->type = type;
   compiler->localCount = 0;
   compiler->scopeDepth = 0;
+  compiler->function = newFunction();
   current = compiler;
+
+  Local* local = &current->locals[current->localCount++];
+  local->depth = 0;
+  local->name.start = "";
+  local->name.length = 0;
 }
 
 static void number(bool canAssign) {
@@ -359,8 +380,6 @@ static void namedVariable(Token name, bool canAssign) {
   } else {
     emitBytes(getOp, arg);
   }
-  
-  emitBytes(OP_GET_GLOBAL, arg);
 }
 
 static void variable(bool canAssign) {
@@ -430,9 +449,26 @@ static void block() {
   while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
     declaration();
   }
-  printf("%d\n", parser.previous.type);
 
   consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
+static void function(FunctionType type) {
+  Compiler compiler;
+  initCompiler(&compiler, type);
+  beginScope();
+
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after function name.");
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after parameters.");
+  consume(TOKEN_RIGHT_BRACE, "Expect '{' before function body.");
+  block();
+}
+
+static void funDeclaration() {
+  uint8_t global = parseVariable("Expect function name.");
+  markInitialized();
+  function(TYPE_FUNCTION);
+  defineVariable(global);
 }
 
 static void varDeclaration() {
@@ -525,6 +561,7 @@ static int caseStatement() {
   emitByte(OP_SWITCH_EQUAL);
   int jump = emitJump(OP_JUMP_IF_FALSE);
   emitByte(OP_POP);
+  emitByte(OP_POP);
   
   while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF) && !check(TOKEN_CASE) && !check(TOKEN_DEFAULT)) {
     statement();
@@ -562,6 +599,48 @@ static void switchStatement() {
   for (int i = 0; i < count; i++) {
     patchJump(cases[i]);
   }
+}
+
+static void loopSwitchCase(int start) {
+  consume(TOKEN_CASE, "Expect 'case' after switch.");
+  expression();
+  consume(TOKEN_COLON, "Expect ':' after case value.");
+
+  emitByte(OP_SWITCH_EQUAL);
+  int jump = emitJump(OP_JUMP_IF_FALSE);
+  emitByte(OP_POP);
+  emitByte(OP_POP);
+  
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF) && !check(TOKEN_CASE) && !check(TOKEN_DEFAULT)) {
+    statement();
+  }
+  
+  emitLoop(start);
+  patchJump(jump);
+  emitByte(OP_POP);
+}
+
+static void loopSwitchStatement() {
+  int loopStart = currentChunk()->count;
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after 'loop'.");
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after loop condition.");
+  consume(TOKEN_LEFT_BRACE, "Expect '{' after loop condition."); 
+
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF) && !check(TOKEN_DEFAULT)) {
+    loopSwitchCase(loopStart);
+  }
+
+  if (match(TOKEN_DEFAULT)) {
+    consume(TOKEN_COLON, "Expect ':' after default case.");
+    while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+      statement();
+    }
+    emitLoop(loopStart);
+  }
+
+  emitByte(OP_POP);
+  consume(TOKEN_RIGHT_BRACE, "Expect '}', after loop.");
 }
 
 static void printStatement() {
@@ -610,7 +689,9 @@ static void synchronize() {
 }
 
 static void declaration() {
-  if (match(TOKEN_VAR)) {
+  if (match(TOKEN_FUN)) {
+    funDeclaration();
+  } else if (match(TOKEN_VAR)) {
     varDeclaration();
   } else {
     statement();
@@ -630,6 +711,8 @@ static void statement() {
     whileStatement();
   } else if (match(TOKEN_SWITCH)) {
     switchStatement();
+  } else if (match(TOKEN_LOOPSWITCH)) {
+    loopSwitchStatement();
   } else if (match(TOKEN_LEFT_BRACE)) {
     beginScope();
     block();
@@ -668,11 +751,11 @@ static ParseRule* getRule(TokenType type) {
   return a;
 }
 
-bool compile(const char* source, Chunk* chunk) {
+ObjFunction* compile(const char* source) {
   initScanner(source);
   Compiler compiler;
-  initCompiler(&compiler);
-  compilingChunk = chunk;
+  initCompiler(&compiler, TYPE_SCRIPT);
+  //compilingChunk = chunk;
 
   parser.hadError = false;
   parser.panicMode = false;
@@ -683,6 +766,6 @@ bool compile(const char* source, Chunk* chunk) {
     declaration();
   }
   
-  endCompiler();
-  return !parser.hadError;
+  ObjFunction* function = endCompiler();
+  return parser.hadError ? NULL : function;
 }
--- scanner.c
+++ scanner.c
@@ -127,6 +127,7 @@ static TokenType identifierType() {
       }
       break;
     case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
+    case 'l': return checkKeyword(1, 9, "oopSwitch", TOKEN_LOOPSWITCH);
     case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
     case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
     case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
